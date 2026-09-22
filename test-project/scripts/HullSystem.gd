b08f599e0dc14f24b47acaf57a09a9845a197026
Jeffrey Popek <jeffrey.popek@gmail.com>
2026-09-22 12:17:54 -0700

    Basic Hull Mechanics
    text only hull system working. hull breaches and repairs

---
+++ test-project/scripts/HullSystem.gd
@@ -0,0 +1,86 @@
+extends Node
+
+# hull
+var max_hull_integrity = 100
+var current_hull_integrity
+var max_flood_level = 100
+var current_flood_level
+var breached
+
+# timer
+var time_elapsed: float = 0.0
+
+# crew
+var current_crew_count = 1
+var available_crew
+
+# repair
+var repair_time: float = 5.0
+var repair_crew_required = 1
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	current_hull_integrity = max_hull_integrity
+	print(current_hull_integrity)
+	
+	current_flood_level = 0
+	available_crew = current_crew_count	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	time_elapsed += delta
+	
+	# hull integrity decay counter
+	if time_elapsed >= 1.0:
+		time_elapsed = 0.0
+		take_damage(1) 
+		
+		if breached:
+			flood_event()
+		
+	#take_damage(1)
+	#print("Hull Integrity: ", current_hull_integrity)
+	if Input.is_key_pressed(KEY_E):
+		repair_hull()
+	
+		
+	
+		
+	
+
+func take_damage(amount):
+	if breached:
+		return
+		
+	current_hull_integrity -= amount
+	print("Hull Integrity: ", current_hull_integrity)
+	
+	if current_hull_integrity <= 0:
+		print("Hull breach! Please fix!")
+		breached = true
+	
+func flood_event():
+	current_flood_level += 10 #change later to a flood rate 
+	
+	print("Water Levels: ", current_flood_level, "/", max_flood_level)
+	
+	if(current_flood_level == max_flood_level):
+		breached = false
+		print("Sub has sunk. Game Over")
+	
+func repair_hull():
+	if available_crew < 1:
+		print("everyone is busy...")
+		return;
+		#
+	available_crew -= 1
+	
+	print("starting repairs...")
+	
+	await get_tree().create_timer(2.0).timeout
+	
+	available_crew += 1
+	current_hull_integrity = max_hull_integrity
+	
+	print("Hull Repaired. Hull Integrity: ", current_hull_integrity, "/", max_hull_integrity)
